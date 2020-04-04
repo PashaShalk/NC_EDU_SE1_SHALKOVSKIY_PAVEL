c9c9394b585e45188ee5d39271d1453905154134
Pavel Shalkovskiy <p.shalkovskiy@gmail.com>
2020-04-04 19:13:18 +0300

    Refactor and Update registration component (cross field validation and more)

---
+++ frontend/src/app/components/registration/custom-validator.ts
@@ -0,0 +1,31 @@
+import { FormGroup, FormControl, FormGroupDirective, NgForm, ValidatorFn } from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/**
+ * Custom validator functions for reactive form validation
+ */
+export class CustomValidator {
+  /**
+   * Validates that child controls in the form group are equal
+   */
+  static childrenEqual: ValidatorFn = (formGroup: FormGroup) => {
+    const password = formGroup.get('password');
+    const confirmPassword = formGroup.get('confirmPassword');
+    if (password.pristine || confirmPassword.pristine) {
+      return null;
+    }
+    return password && confirmPassword && password.value !== confirmPassword.value ? { passwordsMismatch: true } : null;
+    // const [firstControlName, ...otherControlNames] = Object.keys(formGroup.controls || {});
+    // const isValid = otherControlNames.every(controlName => formGroup.get(controlName).value === formGroup.get(firstControlName).value);
+    // return isValid ? null : { childrenNotEqual: true };
+  }
+}
+
+/**
+ * Custom ErrorStateMatcher which returns true (error exists) when the parent form group is invalid and the control has been touched
+ */
+export class ConfirmValidParentMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return control.parent.invalid && control.touched;
+  }
+}
--- frontend/src/app/components/registration/registration.component.ts
+++ frontend/src/app/components/registration/registration.component.ts
@@ -1,22 +1,49 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+import {ConfirmValidParentMatcher, CustomValidator} from './custom-validator';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
+
 export class RegistrationComponent implements OnInit {
 
-  hide;
+  hide: boolean;
   registrationForm: FormGroup;
+  confirmValidParentMatcher = new ConfirmValidParentMatcher();
 
   ngOnInit(): void {
-      this.hide = true;
-      this.registrationForm = new FormGroup({
-        username: new FormControl(''),
-        password: new FormControl(''),
-      });
+    this.hide = true;
+    this.registrationForm = new FormGroup({
+        email: new FormControl('', [
+            Validators.required,
+            Validators.email,
+            Validators.minLength(5),
+            Validators.maxLength(254)]),
+        nickname: new FormControl('', [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(30)]),
+        firstName: new FormControl('', [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(30)]),
+        lastName: new FormControl('', [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(30)]),
+        aboutMyself: new FormControl(''),
+        passwordGroup: new FormGroup({
+            password: new FormControl('', [
+                Validators.required,
+                Validators.minLength(5),
+                Validators.maxLength(30)]),
+            confirmPassword: new FormControl('', [
+                Validators.required
+            ])
+      }, {validators: CustomValidator.childrenEqual }),
+    });
   }
 }
-
