fb6ed46d764a163e95f8938781f7467c7bd81abb
Pavel Shalkovskiy <p.shalkovskiy@gmail.com>
2020-04-08 17:02:53 +0300

    Add like count and refactor image-carousel

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,35 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule} from '@angular/material/button';
-import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {FooterComponent} from './components/footer/footer.component';
+import {LoginComponent} from './components/login/login.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
-import { LoginPageComponent } from './pages/login/login-page.component';
-import { RegistrationComponent } from './components/registration/registration.component';
+import {LoginPageComponent} from './pages/login/login-page.component';
+import {RegistrationComponent} from './components/registration/registration.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import { RegistrationPageComponent } from './pages/registration/registration-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { PostComponent } from './components/post/post.component';
-import { PostMenuComponent } from './components/post-menu/post-menu.component';
-import { MatMenuModule} from '@angular/material/menu';
+import {RegistrationPageComponent} from './pages/registration/registration-page.component';
+import {SearchBoxComponent} from './components/search-box/search-box.component';
+import {PostComponent} from './components/post/post.component';
+import {PostMenuComponent} from './components/post-menu/post-menu.component';
+import {MatMenuModule} from '@angular/material/menu';
 import {RouterModule} from '@angular/router';
 import {Routes} from './routes/routes';
-import { FeedPageComponent } from './pages/feed/feed-page.component';
+import {FeedPageComponent} from './pages/feed/feed-page.component';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
-import { ReportDialogComponent } from './components/report-dialog/report-dialog.component';
+import {ReportDialogComponent} from './components/report-dialog/report-dialog.component';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import { ImageCarouselComponent } from './components/image-carousel/image-carousel.component';
+import {ImageCarouselComponent} from './components/image-carousel/image-carousel.component';
 
 @NgModule({
   declarations: [
